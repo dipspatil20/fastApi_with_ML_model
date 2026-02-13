27df97820ed1e91ff579ab2251971594b9224439
dipalee <109855353+dipspatil20@users.noreply.github.com>
2026-02-13 10:49:30 +0530

    new endpoint

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+
+    return data
+
 @app.get("/")
 def hello():
-    return {'message':'Hello World'}
+    return {'message':'Patient Managment System API'}
 
 @app.get("/about")
 def about():
-    return {'message':'You are welcom to our channel'}
+    return {'message':'A fully functional API to manage your patient records'}
+
+@app.get('/view')
+def view():
+    data = load_data()
+    
+    return data
